9c17955c70d20a3435841e60aeaeb4c0f49604a8
kalaikalaivani <kalaivani1022@gmail.com>
2016-07-10 12:59:59 +0530

    Create wer.c

---
+++ wer.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int i=123,j=2,k=3,l=1,a,t;
+scanf("%d",&i);
+printf("%d is a seed of :");
+a=i*j*k*l;
+t=a;
+printf("%d is seed of %d bz %d =%d",i,t,a,t);
+return 0;
+}
